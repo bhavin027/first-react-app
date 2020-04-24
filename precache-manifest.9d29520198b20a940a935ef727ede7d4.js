6a86988f6d1b4c6b09342fe666ab7059cef76d1e
bhavin027 <bhavinghoghari27@gmail.com>
2020-04-24 20:45:12 +0530

    Updates

---
+++ precache-manifest.9d29520198b20a940a935ef727ede7d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ceb31b10e9919f90c4ae55802e7e7f",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "506be4c9d1270917d775",
+    "url": "/first-react-app/static/css/main.3027d214.chunk.css"
+  },
+  {
+    "revision": "bea828b62df36979d74e",
+    "url": "/first-react-app/static/js/2.c4a5183e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-app/static/js/2.c4a5183e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506be4c9d1270917d775",
+    "url": "/first-react-app/static/js/main.994f61f8.chunk.js"
+  },
+  {
+    "revision": "5de743085609738cb90d",
+    "url": "/first-react-app/static/js/runtime-main.5583dcba.js"
+  }
+]);
